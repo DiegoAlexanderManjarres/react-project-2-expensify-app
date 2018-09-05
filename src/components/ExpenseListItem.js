938dff2daf9d297cd6a2bebf010953b49ec1faa4
DiegoAlexanderManjarres <renmasuo@hotmail.com>
2018-09-04 21:04:22 -0400

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,19 +1,22 @@
 // Export a Stateless functional component
 // description, amount, createdAt
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import numeral from 'numeral'
 
 export const ExpenseListItem = ({id, description, amount, createdAt}) => {
-  return (    
+   return (    
 	  	<li  data-id={id}>
-				<h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-				<p>
-					<strong>Amount:</strong> {amount}  
-					<strong>CreatedAt:</strong> {createdAt}
-				</p>        
+			<h3><Link to={`/edit/${id}`}>{description}</Link></h3>
+			<p>
+				<strong> Amount: </strong> 
+					{numeral(amount / 100).format('$0,0.00')} 
+				<strong> CreatedAt: </strong> 
+					{moment(createdAt).format('MMMM Do, YYYY')}
+			</p>        
 	  	</li>   
-  )
+   )
 }
 export default connect()(ExpenseListItem);
