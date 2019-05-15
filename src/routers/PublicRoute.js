d029a89b88157a08a926c5dd6555312ea2458051
Diego Manjarres <36136246+DiegoAlexanderManjarres@users.noreply.github.com>
2019-05-15 18:51:24 -0400

    Update PublicRoute.js

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom'
 export const PublicRoute = (props) => {
    const { isAuthenticated, component: Component, ...rest } = props   
    return (
-      <Route {...rest} component={props => (
+      <Route {...rest} render={props => (
          isAuthenticated 
          ? (<Redirect to="/Dashboard" />) : (<Component {...props} />) 
       )} />
